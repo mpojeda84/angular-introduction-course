1ca973f175305d7621ec7be38bb6b8d9bf933a13
mpojeda84 <maikel.pereira@gmail.com>
2015-12-28 22:04:15 -0500

    re-organizing

--- example01/app/app.js
+++ example01/app/app.js
@@ -3,30 +3,8 @@
 console.log("Initializing Main Module ...");
 
 // Declare app level module
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.passengerList',
-  'myApp.editPassenger'
-]).
+angular.module('myApp', []).
 
     config(['$routeProvider', function($routeProvider) {
-
-      console.log("Executing App Config");
-
-      $routeProvider.when('/home', {
-        templateUrl: 'home/home.html'
-      });
-
-      $routeProvider.when('/passengers/edit', {
-        templateUrl: 'edit-passenger/edit-passenger.html',
-        controller: 'editPassengerCtrl'
-      });
-
-      $routeProvider.when('/passengers/list', {
-        templateUrl: 'passenger-list/passenger-list.html',
-        controller: 'passengerListCtrl'
-      });
-
-      $routeProvider.otherwise({redirectTo: '/home'});
 
     }]);
